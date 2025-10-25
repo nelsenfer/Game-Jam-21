622b14d82c788f8d3377af87d08161869f196947
Rasyid536 <rasyiddwi031@gmail.com>
2025-10-25 17:19:03 +0700

    luamayan lah

--- Assets/script/Player.cs
+++ Assets/script/Player.cs
@@ -19,10 +19,17 @@ public class Player : MonoBehaviour
     private Vector3 jumpStartPos;
     private Vector3 jumpEndPos;
     public GameObject DeathCanvas;
+private Vector3 zeroVector = Vector3.zero;
+
+    SpriteRenderer sr;
+
+    Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -51,6 +58,7 @@ public class Player : MonoBehaviour
         // jika sudah kembali ke air, reset izin lompat
         if (transform.position.y < 2.3f)
         {
+            animator.SetBool("water", true);
             rb.gravityScale = 0;
             rb.velocity = Vector2.zero;
             canJump = true;
@@ -69,6 +77,24 @@ public class Player : MonoBehaviour
         float inputX = Input.GetAxis("Horizontal");
 
         moveDirection = new Vector3(inputX, inputY, 0f);
+        if (inputX != 0 || inputY != 0)
+        {
+            animator.SetBool("walk", true);
+        }
+
+        else if (inputX == 0 && inputY == 0)
+        {
+            animator.SetBool("walk", false);
+        }
+
+        if (inputX < 0)
+        {
+            sr.flipX = true;
+        }
+        else if (inputX > 0)
+        {
+            sr.flipX = false;
+        }
     }
 
     // Mendeteksi kapal terdekat
@@ -95,6 +121,8 @@ public class Player : MonoBehaviour
     void StartJump(Vector3 targetPos)
     {
         if (isJumping) return;
+        
+        animator.SetBool("water", false);
 
         isJumping = true;
         canJump = false;
